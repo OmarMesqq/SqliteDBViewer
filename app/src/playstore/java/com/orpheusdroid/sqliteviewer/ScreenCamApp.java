4f464ab23f86fa451f4aa4bc73b704250006d501
vijai chander <vijain7000@gmail.com>
2018-07-07 15:19:56 +0530

    Extend BaseApplication class to init shellUtils

--- app/src/playstore/java/com/orpheusdroid/sqliteviewer/ScreenCamApp.java
+++ app/src/playstore/java/com/orpheusdroid/sqliteviewer/ScreenCamApp.java
@@ -17,30 +17,18 @@
 
 package com.orpheusdroid.sqliteviewer;
 
-import android.app.Application;
-import android.content.Context;
-import android.util.Log;
-
-import com.topjohnwu.superuser.BusyBox;
-import com.topjohnwu.superuser.Shell;
-
 import org.solovyev.android.checkout.Billing;
-
-import static com.orpheusdroid.sqliteviewer.Const.TAG;
 
 /**
  * Todo: Add class description here
  *
  * @author Vijai Chandra Prasad .R
  */
-public class ScreenCamApp extends Shell.ContainerApp {
+public class ScreenCamApp extends BaseApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Shell.setFlags(Shell.FLAG_REDIRECT_STDERR);
-        //Shell.verboseLogging(BuildConfig.DEBUG);
-        Shell.setInitializer(ExampleInitializer.class);
     }
 
     private static ScreenCamApp sInstance;
@@ -62,21 +50,5 @@ public class ScreenCamApp extends Shell.ContainerApp {
 
     public Billing getBilling() {
         return mBilling;
-    }
-
-    private static class ExampleInitializer extends Shell.Initializer {
-        @Override
-        public boolean onShellInit(Context context, Shell shell) {
-            Log.d(TAG, "onShellInit");
-            // Use internal busybox
-            BusyBox.setup(context);
-            return true;
-        }
-
-        @Override
-        public boolean onRootShellInit(Context context, Shell shell) {
-            Log.d(TAG, "onRootShellInit");
-            return true;
-        }
     }
 }
